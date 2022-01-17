b79b758be9ffdc62b0d28cf4cca5e6ccba522535
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2022-01-17 14:26:37 +0100

    added assessment greyed out

--- src/components/pages/Learn/Learn.js
+++ src/components/pages/Learn/Learn.js
@@ -14,28 +14,7 @@ const Learn = () => {
 
 			<section className={classes.MainSection}>
 				<h2 className={classes.PageHeader}>All Courses </h2>
-				{/* <div className={classes.CourseInfo}>
-					<h3>Course 1</h3>
-					<p>
-						The Art of Coaching course will equip you with tools to develop your coaching skills and
-						work towards becoming the sort of leader that people would choose to be led by.{' '}
-					</p>
-					<Accordion flush className={classes.Accordion}>
-						<Accordion.Item eventKey="0">
-							<Accordion.Header className={classes.AccordionHeader}>
-								Learning Objectives
-							</Accordion.Header>
-							<Accordion.Body className={classes.AccordionBody}>
-								<ul></ul>
-							</Accordion.Body>
-						</Accordion.Item>
-					</Accordion>
-					<ProgressBar now={50} label={`Progress: ${0} of 7 - ${0}%`} />
-					<div className={classes.ButtonContainer}>
-						<Link to="/">View Course</Link>
-						<Button>Download Certificate</Button>
-					</div>
-				</div> */}
+
 				<div className={classes.CourseContainer}>
 					<CourseCard
 						courseTitle="Course One"
@@ -49,6 +28,21 @@ const Learn = () => {
 						work towards becoming the sort of leader that people would choose to be led by."
 						courseID="3"
 					/>
+				</div>
+
+				<div className={classes.Assessment}>
+					<h2 className={classes.Heading}>Assessment</h2>
+					<div className={classes.CourseInfo}>
+						{/* <h3>Course Assessment</h3> */}
+						<p>
+							Take the course assessment and get access to your certificate when you pass the
+							assessment
+						</p>
+						<div className={classes.ButtonContainer}>
+							<Link to="/">Take Assessment</Link>
+							<Button>Download Certificate</Button>
+						</div>
+					</div>
 				</div>
 
 				{/* {loading && <Loader />}
