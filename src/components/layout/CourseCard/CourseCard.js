6b028be4bc316db19e62e8bc578b9d76b9d0bf40
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2022-01-19 08:53:49 +0100

    changed user course card

--- src/components/layout/CourseCard/CourseCard.js
+++ src/components/layout/CourseCard/CourseCard.js
@@ -2,6 +2,11 @@ import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import classes from './CourseCard.module.css';
 
+const images = {
+	courseOne: 'https://res.cloudinary.com/abisola/image/upload/v1642544624/2473945-min_o0v94k.png',
+	courseTwo: 'https://res.cloudinary.com/abisola/image/upload/v1642544624/5124556-min_cjo8d9.png',
+};
+
 const CourseCard = (props) => {
 	// const idStr = '9';
 	// const code = idStr.charCodeAt(0);
@@ -10,13 +15,16 @@ const CourseCard = (props) => {
 	// console.log(reversed);
 	return (
 		<div className={classes.CourseCard}>
-			<h3 className={classes.CourseTitle}>{props.courseTitle}</h3>
-			<p className={classes.CourseDescription}>{props.courseDescription}</p>
-			<div className={classes.LinkContainer}>
-				<Link to={`/learn/${props.courseID}`}>View Course</Link>
-				<p className={classes.CourseStatus}>
-					Completed: 1 <span>of</span> 1
-				</p>
+			<img src={images[props.image]} alt={props.courseTitle} className={classes.CourseCardImage} />
+			<div>
+				<h3 className={classes.CourseTitle}>{props.courseTitle}</h3>
+				<p className={classes.CourseDescription}>{props.courseDescription}</p>
+				<div className={classes.LinkContainer}>
+					<p className={classes.CourseStatus}>
+						Completed: 1 <span>of</span> 1
+					</p>
+					<Link to={`/learn/${props.courseID}`}>View Course</Link>
+				</div>
 			</div>
 		</div>
 	);
