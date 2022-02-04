e16895fd666a7a2caa14eb8db485509abcd46d2e
“zyhon96” <adedoyin27@gmail.com>
2022-02-04 11:47:54 +0100

    View course Image size reduced and temp link added

--- src/components/layout/HomeCourseCard/HomeCourseCard.js
+++ src/components/layout/HomeCourseCard/HomeCourseCard.js
@@ -5,15 +5,19 @@ const images = {
 	courseOne: 'https://res.cloudinary.com/abisola/image/upload/v1642544624/2473945-min_o0v94k.png',
 	courseTwo: 'https://res.cloudinary.com/abisola/image/upload/v1642544624/5124556-min_cjo8d9.png',
 };
+const data = [];
 
 const HomeCourseCard = (props) => {
+
 	return (
 		<div className={classes.Card}>
 			<img src={images[props.image]} alt={props.title} className={classes.CourseImage} />
 			<div className={classes.CourseTextContainer}>
 				<h5>{props.title}</h5>
 				<p>{props.description}</p>
-				<Link to={`/course/${props.title}`}>View Course &#8250;</Link>
+				<Link to={`/learn/${props.courseID}`}
+				state={{ from: data, props }}
+				>View Course &#8250;</Link>
 			</div>
 		</div>
 	);
